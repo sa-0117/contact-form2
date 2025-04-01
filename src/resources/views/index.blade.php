e9296d5322e70e1734702e8ec79ce5b5f3e164f1
sachiko <axs.r03@gmail.com>
2025-04-02 06:46:50 +0900

    view作成完了

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+
+<div class="contact-form__content">
+    <div class="contact-form__heading">
+        <h2>お問い合わせ</h2>
+    </div>
+    <form class="form" action="/">
+        <div class="form-group">
+            <div class="form-group__title">
+                <span class="form-label__item">お名前
+                </span>
+                <span class="form-label__required">必須</span>
+            </div>
+            <div class="form-group__content">
+                <div class="form-input__text">
+                    <input type="text" name="name" value="" placeholder="テスト太郎" />
+                </div>
+                <div class="form-error">
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="form-group__title">
+                <span class="form-label__item">メールアドレス
+                </span>
+                <span class="form-label__required">必須</span>
+            </div>
+
+            <div class="form-group__content">
+                <div class="form-input__text">
+                    <input  type="email" name="email" value="" placeholder="test@example.com" />
+                </div>
+                <div class="form-error">
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="form-group__title">
+                <span class="form-label__item">電話番号
+                </span>
+                <span class="form-label__required">必須</span>
+            </div>
+            <div class="form-group__content">
+                <div class="form-input__text">
+                    <input  type="tel" name="tel" value="" placeholder="0901234567" />
+                </div>
+                <div class="form-error">
+                </div>
+            </div>
+        </div>    
+        <div class="form-group">
+            <div class="form-group__title">
+                <span class="form-label__item">お問い合わせ内容</span>
+            </div>
+            <div class="form-group__content">
+                <div class="form-textarea__text">
+                    <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-button">
+            <button class="button-submit" type="submit">送信</button>
+        </div>
+    </form>
+</div>
+@endsection
---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+@endsection
+
+@section('content')
+
+<div class="thanks__comment">
+    <div class="thanks__heading">
+        <p>お問い合わせありがとうございます</p>
+    </div>
+</div>
+
+@endsection
